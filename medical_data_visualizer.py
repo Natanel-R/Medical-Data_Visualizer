e8e928ce11e2dbe8bff1b9cbfd40246339651ea7
Natanel-R <natanelr@campus.technion.ac.il>
2025-11-19 00:42:25 +0200

    finish

---
+++ medical_data_visualizer.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Import data
+df = pd.read_csv("medical_examination.csv")
+
+# Add 'overweight' column
+bmi = df['weight'] / ((df['height']/100) ** 2)
+df['overweight'] = bmi > 25
+df['overweight'] = df['overweight'].replace({True: 1, False: 0})
+
+# Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
+df[['gluc', 'cholesterol']] = df[['gluc', 'cholesterol']] == 1
+df['gluc'] = df['gluc'].replace({True: 0, False: 1})
+df['cholesterol'] = df['cholesterol'].replace({True: 0, False: 1})
+
+# Draw Categorical Plot
+def draw_cat_plot():
+    # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
+    df_cat = pd.melt(df, id_vars = "cardio", value_vars = ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+    
+    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
+    df_cat["total"] = 1
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value'], as_index= False).count()
+
+    # Draw the catplot with 'sns.catplot()'
+    fig = sns.catplot(x = "variable", y = "total", data = df_cat, hue="value", kind="bar", col ="cardio").fig
+
+    # Get the figure for the output
+
+    fig.savefig('catplot.png')
+    return fig
+
+
+# Draw Heat Map
+def draw_heat_map():
+    # Clean the data
+    df_heat = df[((df['ap_lo'] <= df['ap_hi']) & ((df['height'] >= df['height'].quantile(0.025))) & ((df['height'] <= df['height'].quantile(0.975)))& ((df['weight'] >= df['weight'].quantile(0.025))) & ((df['weight'] <= df['weight'].quantile(0.975))))]
+
+    # Calculate the correlation matrix
+    corr = df_heat.corr()
+
+    mask = np.triu(corr)
+
+    fig, ax = plt.subplots(figsize = (12,12))
+
+    # Draw the heatmap 
+    sns.heatmap(corr, mask = mask, square = True, annot = True, linewidths=1, fmt=".1f", center = 0.08, cbar_kws = {"shrink": 0.5})
+
+    fig.savefig('heatmap.png')
+    return fig
+
+
+
+cat_fig = draw_cat_plot()
+st.pyplot(cat_fig)
+st.divider()
+
+heat_fig = draw_heat_map()
+st.pyplot(heat_fig)
